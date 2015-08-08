1d1dcc9c45b0c487af7bd22fe2d739d38cc73a20
Lance Vincent Bumanglag <lancevincent@gmail.com>
2015-08-09 02:52:59 +0800

    completed recipe index page

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,7 +3,7 @@ class RecipesController < ApplicationController
 	before_filter :authenticate_user!, except: [:index, :show]
 
 	def index
-		
+		@recipes = Recipe.all.order('created_at desc')
 	end
 
 	def show
