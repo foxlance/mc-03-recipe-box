38a8b472fe76c0db54cdbf4d9c41a33087d1f9b6
Lance Vincent Bumanglag <lancevincent@gmail.com>
2015-08-09 02:38:47 +0800

    added recipe model + paperclip for new recipe/img

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,8 +1,38 @@
 class RecipesController < ApplicationController
+	before_action :find_recipe, only: [:edit, :update, :show, :destroy]
 	before_filter :authenticate_user!, except: [:index, :show]
 
 	def index
 		
+	end
+
+	def show
+		
+	end
+
+	def new
+		@recipe = Recipe.new
+	end
+
+	def create
+		@recipe = current_user.recipes.build(recipe_params)
+
+		if @recipe.save
+			redirect_to @recipe
+		else
+			render 'new'
+		end
+	end
+
+
+	private
+
+	def recipe_params
+		params.require(:recipe).permit(:name, :description, :image)
+	end
+
+	def find_recipe
+		@recipe = Recipe.find(params[:id])
 	end
 
 end
